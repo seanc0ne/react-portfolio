b565a895f0bb0ae4da99d27b2241ca13d7b28471
Sean Cone <seandoesitbetter@gmail.com>
2020-09-30 20:23:05 -0700

    Coding contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+    const { name, email, message } = formState;
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        defaultValue={name}
+                        onChange={handleChange}
+                        name="name"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input
+                        type="text"
+                        defaultValue={email}
+                        onChange={handleChange}
+                        name="email"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <input
+                        type="text"
+                        defaultValue={message}
+                        onChange={handleChange}
+                        name="message"
+                    />
+                </div>
+            </form>
+        </section>
+    )
+}
+
+export default Contact;
