ebfef8ca78ee346e2fb79d13ece81ae0476985a7
Sean Cone <seandoesitbetter@gmail.com>
2020-10-03 20:57:00 -0700

    Fixing Resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,32 +1,14 @@
 import React, { useState } from 'react';
-import { render } from 'react-dom';
-import ResumePdf from 'SeanCone_cv.pdf';
-import { Document, Page } from 'react-pdf';
+import ResumePdf from './SeanCone_cv.pdf';
 
 function Resume() {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-    }
-
     return (
-    //   <section className="my-5">
-    //     <h1 id="about">Resume</h1>
-    //     <div class="section-content">
-    //         <p>Click <a href="../../assets/images/SeanCone_cv.pdf" target="_blank">here</a> to view my resume.</p>
-    //     </div>
-    //   </section>
-    <div>
-    <Document
-      file="SeanCone_cv.pdf"
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      <Page pageNumber={pageNumber} />
-    </Document>
-    <p>Page {pageNumber} of {numPages}</p>
-  </div>
+      <section className="my-5">
+        <h1 id="about">Resume</h1>
+        <div class="section-content">
+            <p>Click <a href={ResumePdf} target="_blank">here</a> to view my resume.</p>
+        </div>
+      </section>
     );
   }
 
